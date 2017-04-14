6b0aede214f1904f28dbab4db27221d2670ecba3
Ming Chung <ming.chung@paconsulting.com>
2017-04-14 14:31:42 +0100

    Adding some basic example code

---
+++ Asset-Algebra/Asset-Algebra/Base-Token.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset_Algebra
+{
+    class Base_Token
+    {
+        public string address;    // Base token issuer address
+        public string description;    // Description e.g. ''Pay bearer one USD''
+        public string hash;     // Hash of legal text defining Token.
+    }
+}
--- Asset-Algebra/Asset-Algebra/Program.cs
+++ Asset-Algebra/Asset-Algebra/Program.cs
@@ -10,6 +10,20 @@ namespace Asset_Algebra
     {
         static void Main(string[] args)
         {
+            //TODO: inset your code for playing with assets here...
+
+            Base_Token my_token = new Base_Token();
+            my_token.address = "some address";
+            my_token.description = "an example token";
+            my_token.hash = "some hash";
+
+            Console.WriteLine("Token address: " + my_token.address);
+            Console.WriteLine("Token description: " + my_token.description);
+            Console.WriteLine("Token hash: " + my_token.hash);
+
+            Console.WriteLine("Press key to exit");
+
+            Console.ReadKey();
         }
     }
 }
